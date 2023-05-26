129cbdfae920adbfce1d0c8428bf8ae46c960138
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-26 18:26:31 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   enable_dns_support = true
   enable_dns_hostnames = true
-  tags = merge(var.tags, { Name = "${var.env}~vpc" })
+  tags = merge(var.tags, { Name = "${var.env}-vpc" })
 }
 
 module "subnets" {
@@ -16,4 +16,10 @@ module "subnets" {
 
   tags = var.tags
   env = var.env
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+tags = merge(var.tags, { Name = "${var.env}-igw" })
 }
