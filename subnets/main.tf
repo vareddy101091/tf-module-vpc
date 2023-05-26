89d1b14ee240b8ccc779c1d659677b4c52333d6b
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-26 17:51:50 +0530

    Ansible

---
+++ subnets/main.tf
@@ -0,0 +1,9 @@
+resource "aws_subnet" "main" {
+  count = length(var.cidr_block)
+  vpc_id = var.vpc_id
+  cidr_block = var.cidr_block[count.index]
+
+  tags = {
+    Name = merge(var.tags, { Name = "${var.env}-${var.name}-${count.index}" })
+  }
+}
