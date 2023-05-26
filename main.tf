d1ec8a1ce706f8afd0edc6ab2047d69072619a34
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-26 18:46:38 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -23,3 +23,17 @@ resource "aws_internet_gateway" "igw" {
 
 tags = merge(var.tags, { Name = "${var.env}-igw" })
 }
+
+resource "aws_eip" "ngw" {
+  count = length(var.subnets["public"].cidr_block)
+  vpc = true
+  tags  = merge(var.tags, { Name = "${var.env}-ngw" })
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count         = length(var.subnets["public"].cidr_block)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = module.subnets["public"].subnet_ids[count.index]
+
+  tags = merge(var.tags, { Name = "${var.env}-ngw" })
+}
