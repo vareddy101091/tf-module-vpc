4e809753f53dc3426b8435525200a1372b9393a9
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-26 18:09:25 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "subnets" {
   vpc_id = aws_vpc.main.id
   cidr_block = each.value["cidr_block"]
   name = each.value["name"]
+  azs = each.value["azs"]
 
   tags = var.tags
   env = var.env
