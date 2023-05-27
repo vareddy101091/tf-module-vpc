cb050a6f50d3336960151438c899b65e9c6daf90
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-27 17:27:22 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -42,4 +42,5 @@ resource "aws_route" "igw" {
   count = length(module.subnets["public"].route_table_ids)
   route_table_id = module.subnets["public"].route_table_ids[count.index]
   gateway_id = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
 }
