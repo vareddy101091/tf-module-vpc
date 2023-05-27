f95b2573643f285e1c4884c6d3fabcd625459100
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-27 17:38:29 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -44,3 +44,11 @@ resource "aws_route" "igw" {
   gateway_id = aws_internet_gateway.igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+
+resource "aws_route" "ngw" {
+  count = length(local.all_private_subnet_ids)
+  route_table_id = local.all_private_subnet_ids[count.index]
+  nat_gateway_id = aws_nat_gateway.ngw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
