dcac3a0db91dcabd329327823b1d7a1719716f63
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-27 17:25:38 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -38,3 +38,8 @@ resource "aws_nat_gateway" "ngw" {
   tags = merge(var.tags, { Name = "${var.env}-ngw" })
 }
 
+resource "aws_route" "igw" {
+  count = length(module.subnets["public"].route_table_ids)
+  route_table_id = module.subnets["public"].route_table_ids[count.index]
+  gateway_id = aws_internet_gateway.igw.id
+}
