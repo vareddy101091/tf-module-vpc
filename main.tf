a2b33d9af6bd2d2268e7abce75bc2b38d8c3842e
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-29 18:09:43 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -54,7 +54,6 @@ resource "aws_route" "ngw" {
 }
 
 resource "aws_vpc_peering_connection" "peer" {
-  peer_owner_id = data.aws_caller_identity.identity.account_id
   peer_vpc_id = var.default_vpc_id
   vpc_id      = aws_vpc.main.id
   auto_accept = true
