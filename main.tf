09689cc97043da8681d485cac05177423b82cab9
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-29 18:54:13 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -58,14 +58,14 @@ resource "aws_vpc_peering_connection" "peer" {
   auto_accept = true
 }
 
-resource "aws_route" "peering-connection-route" {
+resource "aws_route" "peering_connection_route" {
   count = length(local.all_private_subnet_ids)
   route_table_id = element(local.all_private_subnet_ids, count.index)
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   destination_cidr_block = var.default_vpc_cidr
 }
 
-resource "aws_route" "peering-connection-route-in-default-vpc" {
+resource "aws_route" "peering_connection_route_in_default_vpc" {
   route_table_id = var.default_vpc_rtid
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   destination_cidr_block = var.cidr_block
