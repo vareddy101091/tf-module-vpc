038b08447d3d0a5b471c96b10fe91449ce2bd0f3
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-29 16:44:49 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -53,6 +53,3 @@ resource "aws_route" "ngw" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-output "subnets" {
-  value = module.subnets
-}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "subnets" {
+  value = module.subnets
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
